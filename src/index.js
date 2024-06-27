9025fa45ed23678d8f188691d377e8561637c23c
mxriuki <mwendav37@gmail.com>
2024-06-27 14:01:12 +0300

    code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createTask = document.getElementById("newTaskBtn");
+  let inputBox = document.getElementById("new-task-description");
+  const taskBox = document.getElementById("list")
+
+  createTask.addEventListener("click", (e)=>{
+    e.preventDefault();
+    let blam = document.createElement("form"); let deleteBtn = document.createElement("button"); 
+    blam.innerHTML = inputBox.value; deleteBtn.innerHTML = "X"
+    blam.appendChild(deleteBtn); taskBox.appendChild(blam);
+
+    deleteBtn.addEventListener("click",(e)=>{
+      e.preventDefault();
+      blam.remove(); deleteBtn.remove();
+    })
+  })
 });
